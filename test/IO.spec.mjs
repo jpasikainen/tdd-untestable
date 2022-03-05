627cddbc46254e69f5d4150f2f314086be497b0b
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-05 19:46:06 +0200

    improve test

--- test/IO.spec.mjs
+++ test/IO.spec.mjs
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { IO } from "../src/IO.mjs";
+import { readFile } from "fs";
 
 describe("IO", () => {
   let io;
@@ -10,8 +11,16 @@ describe("IO", () => {
     expect(io.fileExists).to.equal(false);
   })
   it("writes time", () => {
+    const writeTime = new Date().getTime();
     io.writeTime();
     expect(io.fileExists).to.equal(true);
+    let time;
+    readFile("time.txt", (err, data) => {
+      time = data.toString();
+    });
+    const diff = (new Date().getTime() - writeTime) / 1000;
+    expect(diff).lessThan(30);
+    // Expect time between write and read to be less than 30 seconds
   });
   it("destorys file", () => {
     io.writeTime();
